9328bc5e7253a9dfeb7d3c805a82570d80bd84a6
RaV <fenji92+github@gmail.com>
2020-05-21 08:44:05 +0200

    World file in env variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ echo "\nBootstrap:\nconfigpath=$CONFIGPATH\nworldpath=$WORLDPATH\nlogpath=$LOGPA
 echo "Copying plugins..."
 cp -Rfv /plugins/* ./ServerPlugins
 
-mono --server --gc=sgen -O=all TerrariaServer.exe -configPath "$CONFIGPATH" -worldpath "$WORLDPATH" -logpath "$LOGPATH" "$@" 
+mono --server --gc=sgen -O=all TerrariaServer.exe -configPath "$CONFIGPATH" -worldpath "$WORLDPATH" -logpath "$LOGPATH" -world "$WORLD" "$@" 
