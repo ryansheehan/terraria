f5ba44420e0554e362a5cb47e193b12a9250be14
velimir-97 <velimir@wickitly.com>
2022-01-14 14:55:18 +0100

    Added server config file for tshock.

--- tshock/bootstrap.sh
+++ tshock/bootstrap.sh
@@ -17,20 +17,25 @@ if [ $STORAGETYPE = "mysql" ]; then
 fi
 
 WORLD_PATH="/root/.local/share/Terraria/Worlds/$WORLD_FILENAME"
+if [ ! -f "/config/serverconfig.txt" ]; then
+    echo "Server configuration not found, running with default server configuration."
+    echo "Please ensure your desired serverconfig.txt file is volumed into docker: -v <path_to_config_file>:/config"
+    cp ./serverconfig-default.txt /config/serverconfig.txt
+fi
 
 if [ -z "$WORLD_FILENAME" ]; then
   echo "No world file specified in environment WORLD_FILENAME."
-  if [ -z "$@" ]; then 
+  if [ -z "$@" ]; then
     echo "Running server setup..."
   else
     echo "Running server with command flags: $@"
   fi
-  mono --server --gc=sgen -O=all TerrariaServer.exe -configpath "$CONFIGPATH" -logpath "$LOGPATH" "$@" 
+  mono --server --gc=sgen -O=all TerrariaServer.exe -configpath "$CONFIGPATH" -logpath "$LOGPATH" -config "$CONFIG" "$@"
 else
   echo "Environment WORLD_FILENAME specified"
   if [ -f "$WORLD_PATH" ]; then
     echo "Loading to world $WORLD_FILENAME..."
-    mono --server --gc=sgen -O=all TerrariaServer.exe -configpath "$CONFIGPATH" -logpath "$LOGPATH" -world "$WORLD_PATH" "$@" 
+    mono --server --gc=sgen -O=all TerrariaServer.exe -configpath "$CONFIGPATH" -logpath "$LOGPATH" -config "$CONFIG" -world "$WORLD_PATH" "$@"
   else
     echo "Unable to locate $WORLD_PATH.\nPlease make sure your world file is volumed into docker: -v <path_to_world_file>:/root/.local/share/Terraria/Worlds"
     exit 1
