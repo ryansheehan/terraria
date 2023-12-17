5b0c674073e218d71ebb25c28cca6e7e19465538
Logan Saso <logansaso+tech@gmail.com>
2023-12-17 01:30:33 -0800

    The autocreate flag will create a world in place if WORLD_FILENAME is set

--- tshock/bootstrap.sh
+++ tshock/bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "\nBootstrap:\nworld_file_name=$WORLD_FILENAME\nconfigpath=$CONFIGPATH\nlogpath=$LOGPATH\n"
 echo "Copying plugins..."
@@ -17,6 +17,15 @@ fi
 
 WORLD_PATH="/root/.local/share/Terraria/Worlds/$WORLD_FILENAME"
 
+autocreate_flag=false
+for arg in "$@"; do
+    if [ "$arg" = "-autocreate" ]; then
+        autocreate_flag=true
+        break
+    fi
+done
+
+
 if [ -z "$WORLD_FILENAME" ]; then
   echo "No world file specified in environment WORLD_FILENAME."
   if [ -z "$@" ]; then 
@@ -27,11 +36,13 @@ if [ -z "$WORLD_FILENAME" ]; then
   ./TShock.Server -configpath "$CONFIGPATH" -logpath "$LOGPATH" "$@" 
 else
   echo "Environment WORLD_FILENAME specified"
-  if [ -f "$WORLD_PATH" ]; then
-    echo "Loading to world $WORLD_FILENAME..."
-    ./TShock.Server -configpath "$CONFIGPATH" -logpath "$LOGPATH" -world "$WORLD_PATH" "$@" 
+  if [ -f "$WORLD_PATH" ] || [ "$autocreate_flag" = true ]; then
+      echo "Loading to world $WORLD_FILENAME..."
+      ./TShock.Server -configpath "$CONFIGPATH" -logpath "$LOGPATH" -world "$WORLD_PATH" "$@"
   else
-    echo "Unable to locate $WORLD_PATH.\nPlease make sure your world file is volumed into docker: -v <path_to_world_file>:/root/.local/share/Terraria/Worlds"
-    exit 1
+      echo "Unable to locate $WORLD_PATH and -autocreate flag is not set."
+      echo "Please make sure your world file is volumed into docker: -v <path_to_world_file>:/root/.local/share/Terraria/Worlds"
+      echo "Alternatively, use the -autocreate flag to create a new world."
+      exit 1
   fi
 fi
